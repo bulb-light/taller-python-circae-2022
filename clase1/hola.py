7d2a580f094b8ae9cfd71d3992a9d37f623404ab
bulb-light <182960@unsaac.edu.pe>
2022-07-07 22:07:20 -0500

    Primera clase

---
+++ clase1/hola.py
@@ -0,0 +1,27 @@
+# Este es mi primer programa
+# tipos de datos inmutables en Python
+# string --> str
+# entero --> int
+# reales --> float
+# print() --> muestra informacion en la consola
+# input() --> esta funcion permite obtener datos
+#             ingresados por el usuario a traves
+#             de la consola
+# type(var) --> esta funcion determina el tipo de dato
+#            de la variable "var" 
+
+# asignando la entrada del usuario
+# a la variable "user_input"
+user_input = input("Escriba algo: ")
+print(type(user_input))
+print(user_input)
+
+# creando y asignando variables
+mivariable = "hola mundo!"
+entero = 10
+decimal = 12.2
+# mostrando en la consolda el tip
+# de dato de las tres variables definidas arriba
+print(type(mivariable))
+print(type(entero))
+print(type(decimal))
